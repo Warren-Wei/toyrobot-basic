5243db38f5b99cd583bfc7108e619ba4f0c6ee5a
Warren Wei <warrenwei1108@gmail.com>
2019-07-20 18:53:06 +1000

    Add onCommandLeft

--- robot.js
+++ robot.js
@@ -1,6 +1,6 @@
 
 function onCommandRight() {
-    var mapCells = document.querySelectorAll('.map-cell');
+    var mapCells = document.querySelectorAll(".map-cell");
     var currentRobotLocation = 0;
     for (var i = 0; i < mapCells.length; i++) {
         if (mapCells[i].innerHTML.length > 0) {
@@ -8,7 +8,21 @@ function onCommandRight() {
             break;
         }
     }
-    mapCells[currentRobotLocation].innerHTML = '';
+    mapCells[currentRobotLocation].innerHTML = "";
     // todo check the available of the target position
-    mapCells[currentRobotLocation + 1].innerHTML = 'R';
+    mapCells[currentRobotLocation + 1].innerHTML = "R";
+}
+
+function onCommandLeft() {
+    var mapCells = document.querySelectorAll(".map-cell");
+    var currentRobotLocation = 0;
+    for (var i = 0; i < mapCells.length; i++) {
+        if (mapCells[i].innerHTML.length > 0) {
+            currentRobotLocation = i;
+            break;
+        }
+    }
+    mapCells[currentRobotLocation].innerHTML = "";
+    // todo check the available of the target position
+    mapCells[currentRobotLocation - 1].innerHTML = "R";
 }
