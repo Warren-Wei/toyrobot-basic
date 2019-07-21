8d4ceef905d9d10586feb5fafee5b4f465167790
Warren Wei <warrenwei1108@gmail.com>
2019-07-21 20:19:09 +1000

    Array munipulation

--- robot.js
+++ robot.js
@@ -18,6 +18,19 @@ function move(newPosition) {
     if (availablePosition(newPosition, state.mapSize)) {
         var oldState = Object.assign({}, state);
         histories.push(oldState);
+        
+        // histories elements: position > 2
+        var greaterThan2 = histories.filter(aState => aState.robotPosition > 2);
+        console.log("greaterThan2: ", greaterThan2);
+        // histories: robotPosition only [2, 3, 4]
+        var posOnly = histories.map(aState => aState.robotPosition);
+        console.log("posOnly: " + posOnly);
+        // combine all the icons 'RRRRR'
+        // var combination = histories.map(state => state.icon).join("").toString();
+        var combination = histories.reduce((total, aState) => total + aState.icon, "");
+        
+        // console.log("combination: " + combination);
+        
         state.robotPosition = newPosition;
         render();
         return true;
